adeeb096605914fe3b9e9d3e7cf23d90700e4069
Eric Ross <epr@evross.com>
2018-11-26 07:18:14 -0800

    Added more samples from Udemy class

--- rdd/airports/AirportsByLatitudeProblem.py
+++ rdd/airports/AirportsByLatitudeProblem.py
@@ -1,4 +1,12 @@
-from pyspark import SparkContext
+import sys
+sys.path.insert(0, '.')
+from pyspark import SparkContext, SparkConf
+from commons.Utils import Utils
+
+
+def splitComma(line: str):
+    splits = Utils.COMMA_DELIMITER.split(line)
+    return "{}, {}".format(splits[1], splits[6])
 
 if __name__ == "__main__":
 
@@ -15,3 +23,12 @@ if __name__ == "__main__":
     "Tofino", 49.082222
     ...
     '''
+    conf = SparkConf().setAppName("airports").setMaster("local[*]")
+    sc = SparkContext(conf = conf)
+
+    airports = sc.textFile("in/airports.text")
+
+    airportsAbove40 = airports.filter(lambda line : float(Utils.COMMA_DELIMITER.split(line)[6]) > 40.0)
+
+    airportsNameAndLat = airportsAbove40.map(splitComma)
+    airportsNameAndLat.saveAsTextFile("out/above40.txt")
--- rdd/airports/AirportsInUsaSolution.py
+++ rdd/airports/AirportsInUsaSolution.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     airports = sc.textFile("in/airports.text")
     airportsInUSA = airports.filter(lambda line : Utils.COMMA_DELIMITER.split(line)[3] == "\"United States\"")
 
-    airportsNameAndCityNames = airportsInUSA.map(splitComma)
-    airportsNameAndCityNames.saveAsTextFile("out/airports_in_usa.text")
+    airportsNameAndLat = airportsInUSA.map(splitComma)
+    airportsNameAndLat.saveAsTextFile("out/airports_above_40.text")
